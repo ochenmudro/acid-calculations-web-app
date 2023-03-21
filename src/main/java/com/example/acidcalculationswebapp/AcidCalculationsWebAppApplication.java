d2134b8f5c81405b7b43627c19fd3ee5854b9349
ochen_mudro <43651215+ochenmudro@users.noreply.github.com>
2023-03-21 16:40:31 +0300

    Initial commit

---
+++ src/main/java/com/example/acidcalculationswebapp/AcidCalculationsWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.acidcalculationswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcidCalculationsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcidCalculationsWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/acidcalculationswebapp/AcidCalculationsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acidcalculationswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcidCalculationsWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
